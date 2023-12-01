ffdbac4169c7195f2a6467b4b336e771a40818b4
Praveen01kumar <pk2414089@gmail.com>
2023-12-01 13:03:30 +0530

    ai kit update

--- src/app/pages/user/blgdetail/blgdetail.component.ts
+++ src/app/pages/user/blgdetail/blgdetail.component.ts
@@ -16,7 +16,7 @@ export class BlgdetailComponent implements OnInit {
   private highlighted: boolean = false;
   // &lt;div&gt;hello&lt;/div&gt;
   htmlContent1: any = "<pre><code class=\"language-markup\">&lt;p i18n&gt;Hello, World!&lt;/p&gt;\n</code></pre>";
-  htmlContent2: any = '<pre><code class=\"language-typescript\">function Product(name, price) {  // Product constructor    \n    this.name = name;\n    this.price = price;\n}\n\nvar product1 = new Product("Shoes", 49.99); // Product instance\nvar product2 = new Product("Phone", 599.99); // Another product\n\nfunction Person(firstName, lastName) { // Person constructor\n    this.firstName = firstName;\n    this.lastName = lastName;\n}\n\nvar person1 = new Person("John", "Doe"); // Person instance\nvar person2 = new Person("Alice", "Smith"); // Another person\n\nfunction Animal(type, legs) {       // Animal constructor\n    this.type = type;\n    this.legs = legs;\n}\n\nvar cat = new Animal("Cat", 4);      // Cat instance\nvar dog = new Animal("Dog", 4);      // Dog instance\n\nfunction Company(name, employees) {  // Company constructor\n    this.name = name;\n    this.employees = employees;\n}\n\nvar company1 = new Company("ABC Inc.", ["Alice", "Bob"]); // Company instance\nvar company2 = new Company("XYZ Corp.", ["Charlie", "David"]); // Another company\n</pre></code>';
+  htmlContent2: any = '<pre><code class=\"language-typescript\">// Using var (can be reassigned)\n  var arrVar = [1, 2, 3];\n  arrVar.push(4);\n  \n  // Using let (can be reassigned)\n  let arrLet = [1, 2, 3];\n  arrLet.push(4);\n  \n  // Using const (cannot be reassigned, but array elements can be modified)\n  const arrConst = [1, 2, 3];\n  arrConst.push(4);\n   </pre></code>';
   constructor(
     private highlightService: HighlightService,
     private apiService: ApiService,
@@ -47,19 +47,17 @@ export class BlgdetailComponent implements OnInit {
     if (res) {
       const data = res?.topicsArr?.find((el: any) => el?.title === postCt);
       this.postDetail = data?.left.concat(data?.right)?.find((el: any) => el?.name === postId);
-      // show detail view 
       if (this.postDetail) {
         const postContainer = document.getElementById('discription') as HTMLElement;
         const titleElement = document.createElement('h2');
         titleElement.textContent = this.postDetail?.name;
         const descriptionElement = document.createElement('div');
-        descriptionElement.innerHTML = this.postDetail?.discription!==undefined?this.postDetail?.discription:'Comming Soon...';
+        descriptionElement.innerHTML = this.postDetail?.discription !== undefined ? this.postDetail?.discription : 'Comming Soon...';
         postContainer.appendChild(titleElement);
         postContainer.appendChild(descriptionElement);
       }
     }
   }
-
 
   ngAfterViewChecked() {
     this.highlightAll();
