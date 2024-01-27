698fa11e065df7e8955cb2e856fc74ecd39db2f7
Praveen01kumar <pk2414089@gmail.com>
2024-01-27 22:13:40 +0530

    action button added to pdf viewer pages

--- src/app/pages/more/pdfbooks/pdfbooks.component.ts
+++ src/app/pages/more/pdfbooks/pdfbooks.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
 @Component({
   selector: 'pdfbooks',
   templateUrl: './pdfbooks.component.html',
@@ -14,61 +14,53 @@ export class PdfbooksComponent implements OnInit {
   pageOrgSize: boolean = false;
   pageAll: boolean = false;
   pageText: boolean = true;
-
+  @ViewChild(PdfViewerComponent, { static: false }) private pdfComponent?: PdfViewerComponent | undefined;
+  searchQuery: string = '';
+  maxPageValue: number | null = null;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  goToPage(event: any) {
+    const enteredValue = parseInt(event.target.value, 10);
+    if (this.maxPageValue !== null) {
+      if (!isNaN(enteredValue)) { if (enteredValue >= this.maxPageValue) { this.pageNumber = this.maxPageValue; } else { this.pageNumber = enteredValue; } };
+      if (isNaN(enteredValue)) { this.pageNumber = 1; };
+    }
+  }
+
+  searchInPdf() {
+    if (this.pdfComponent) {
+      this.pdfComponent.eventBus.dispatch('find', {
+        query: this.searchQuery, type: 'again', caseSensitive: true, findPrevious: undefined, highlightAll: true, phraseSearch: true
+      });
+    }
   }
 
   zoomPage(idv: string) {
-    if (idv === '-') {
-      if (this.pageZoom > 0.2) {
-        this.pageZoom = this.pageZoom - 0.1;
-      }
-    }
-    if (idv === '+') {
-      if (this.pageZoom < 1) {
-        this.pageZoom = this.pageZoom + 0.1;
-      }
-    }
-  }
-  zoomScale(scale: string) {
-    this.pageScale = scale;
+    if (idv === '-') { if (this.pageZoom > 0.2) { this.pageZoom = this.pageZoom - 0.1; } };
+    if (idv === '+') { if (this.pageZoom < 1) { this.pageZoom = this.pageZoom + 0.1; } };
   }
 
-  originalSize() {
-    this.pageOrgSize = !this.pageOrgSize;
+  zoomScale(scale: string) { this.pageScale = scale; };
+  originalSize() { this.pageOrgSize = !this.pageOrgSize; };
+  showAll() { this.pageAll = !this.pageAll; };
+  renderText() { this.pageText = !this.pageText; };
+  nextPage() { this.pageNumber++; };
+  previousPage() { if (this.pageNumber > 1) { this.pageNumber--; } };
+
+  afterLoadComplete(pdf: any): void {
+    this.maxPageValue = pdf.numPages;
+    console.log('after-load-complete', pdf);
   }
 
-  showAll() {
-    this.pageAll = !this.pageAll;
+  pageRendered(page: CustomEvent) {
+    console.log('(page-rendered)', page);
   }
 
-  renderText() {
-    this.pageText = !this.pageText;
-  }
-
-  nextPage() {
-    this.pageNumber++;
-  }
-
-  previousPage() {
-    if (this.pageNumber > 1) {
-      this.pageNumber--;
-    }
-  }
-
-  afterLoadComplete(pdf: any) {
-    console.log('after-load-complete');
-  }
-
-  pageRendered(e: CustomEvent) {
-    console.log('(page-rendered)', e);
-  }
-
-  textLayerRendered(e: CustomEvent) {
-    console.log('(text-layer-rendered)', e);
+  textLayerRendered(rendered: CustomEvent) {
+    console.log('(text-layer-rendered)', rendered);
   }
 
   onFileSelected(event: any) {
